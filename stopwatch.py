09ef9fca7793243f576ae6cbbcfe1904ac465c06
Mohamed El Wali <muhamedelwali@gmail.com>
2024-01-11 22:30:54 +0100

    add 'start case'/'stop case'

--- stopwatch.py
+++ stopwatch.py
@@ -8,7 +8,7 @@ class Stopwatch(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('Stopwatch')
+        self.setWindowTitle('Case Timer')
         self.setGeometry(100, 100, 200, 100)
 
         self.layout = QVBoxLayout()
@@ -16,17 +16,13 @@ class Stopwatch(QMainWindow):
         self.label = QLabel('00:00:00')
         self.layout.addWidget(self.label)
 
-        self.startButton = QPushButton('Start')
-        self.startButton.clicked.connect(self.startTimer)
-        self.layout.addWidget(self.startButton)
+        self.startCaseButton = QPushButton('Start Case')
+        self.startCaseButton.clicked.connect(self.startTimer)
+        self.layout.addWidget(self.startCaseButton)
 
-        self.stopButton = QPushButton('Stop')
-        self.stopButton.clicked.connect(self.stopTimer)
-        self.layout.addWidget(self.stopButton)
-
-        self.resetButton = QPushButton('Reset')
-        self.resetButton.clicked.connect(self.resetTimer)
-        self.layout.addWidget(self.resetButton)
+        self.stopCaseButton = QPushButton('Stop Case')
+        self.stopCaseButton.clicked.connect(self.resetTimer)
+        self.layout.addWidget(self.stopCaseButton)
 
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.updateTime)
@@ -37,27 +33,26 @@ class Stopwatch(QMainWindow):
         container.setLayout(self.layout)
         self.setCentralWidget(container)
 
+        self.updateButtons(False)
+
     def startTimer(self):
         self.timer.start(1000)
-        self.startButton.setEnabled(False)
-        self.stopButton.setEnabled(True)
-
-    def stopTimer(self):
-        self.timer.stop()
-        self.startButton.setEnabled(True)
-        self.stopButton.setEnabled(False)
+        self.updateButtons(True)
 
     def resetTimer(self):
         self.timer.stop()
         self.counter = 0
         self.label.setText('00:00:00')
-        self.startButton.setEnabled(True)
-        self.stopButton.setEnabled(False)
+        self.updateButtons(False)
 
     def updateTime(self):
         self.counter += 1
         time_str = f'{self.counter // 3600:02d}:{(self.counter // 60) % 60:02d}:{self.counter % 60:02d}'
         self.label.setText(time_str)
+
+    def updateButtons(self, isRunning):
+        self.startCaseButton.setEnabled(not isRunning)
+        self.stopCaseButton.setEnabled(isRunning)
 
 def main():
     app = QApplication(sys.argv)
