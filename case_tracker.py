cb8cd5ff9516eef072a00555d73a98ede06d971d
Mohamed El Wali <muhamedelwali@gmail.com>
2024-01-11 23:06:07 +0100

    add database

--- case_tracker.py
+++ case_tracker.py
@@ -1,4 +1,6 @@
 import sys
+import sqlite3
+from datetime import datetime
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QPushButton, QWidget
 from PyQt5.QtCore import QTimer
 
@@ -6,6 +8,8 @@ class Stopwatch(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.initDB()
+        self.current_case_id = None
 
     def initUI(self):
         self.setWindowTitle('Case Timer')
@@ -17,11 +21,11 @@ class Stopwatch(QMainWindow):
         self.layout.addWidget(self.label)
 
         self.startCaseButton = QPushButton('Start Case')
-        self.startCaseButton.clicked.connect(self.startTimer)
+        self.startCaseButton.clicked.connect(self.startCase)
         self.layout.addWidget(self.startCaseButton)
 
         self.stopCaseButton = QPushButton('Stop Case')
-        self.stopCaseButton.clicked.connect(self.resetTimer)
+        self.stopCaseButton.clicked.connect(self.stopCase)
         self.layout.addWidget(self.stopCaseButton)
 
         self.timer = QTimer(self)
@@ -35,15 +39,38 @@ class Stopwatch(QMainWindow):
 
         self.updateButtons(False)
 
-    def startTimer(self):
+    def initDB(self):
+        self.conn = sqlite3.connect('case_timer.db')
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS cases (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                case_date TEXT,
+                start_time TEXT,
+                end_time TEXT
+            )
+        ''')
+        self.conn.commit()
+
+    def startCase(self):
         self.timer.start(1000)
         self.updateButtons(True)
+        current_datetime = datetime.now()
+        date_str = current_datetime.strftime('%Y-%m-%d')
+        time_str = current_datetime.strftime('%H:%M:%S')
+        self.cursor.execute("INSERT INTO cases (case_date, start_time) VALUES (?, ?)", (date_str, time_str))
+        self.conn.commit()
+        self.current_case_id = self.cursor.lastrowid
 
-    def resetTimer(self):
+    def stopCase(self):
         self.timer.stop()
         self.counter = 0
         self.label.setText('00:00:00')
         self.updateButtons(False)
+        if self.current_case_id is not None:
+            end_time_str = datetime.now().strftime('%H:%M:%S')
+            self.cursor.execute("UPDATE cases SET end_time = ? WHERE id = ?", (end_time_str, self.current_case_id))
+            self.conn.commit()
 
     def updateTime(self):
         self.counter += 1
